d22a028b274033a9240dd96155a113c77e6dd826
amrmohamed <amoree.mb@gmail.com>
2024-01-25 22:08:46 +0200

    Add styling to pop up form

--- my-first-react-app/src/components/CvDisplay.jsx
+++ my-first-react-app/src/components/CvDisplay.jsx
@@ -1,16 +1,19 @@
+import "../styles/cv.css"
+
 export default function CvDisplay( {generalData, eduData, showGeneralForm, showEduForm} ) {
     return (
         <div>
             <section className="personal-details">
-            <div className="header-and-button">
+            <div className="header-and-button full-bleed">
             <h1>Personal Details</h1>
             <button className="toggle-general-form" onClick={showGeneralForm}>Add</button>
             </div>
             <h2>{generalData.name}</h2>
             {generalData.phone !== "" && <p className="phone-email">Phone: {generalData.phone} | Email: {generalData.email}</p>}
             </section>
+
             <section className="education">
-            <div className="header-and-button">
+            <div className="header-and-button full-bleed">
             <h1>Education</h1>
             <button className="toggle-edu-form" onClick={showEduForm}>Add</button>
             </div>
--- my-first-react-app/src/components/Form.jsx
+++ my-first-react-app/src/components/Form.jsx
@@ -3,8 +3,10 @@ export default function Form({id,title, onSubmit, children, isShown}) {
         <>
         { isShown &&
         <section className="form">
-            <form id={id} action='' onSubmit={onSubmit} className="toggle-form">
+            <form id={id} action='' onSubmit={onSubmit}>
+                
                 <h2>{title}</h2>
+                
                     {children}
                 <button type="submit" form={id} >Submit</button>
             </form>
